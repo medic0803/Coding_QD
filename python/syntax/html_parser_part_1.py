dbdd5622d6580cdbb0ef826e37b353d58bf17c9b
Runfeng Wang <medicwrf@gmail.com>
2022-03-10 22:02:01 +0000

    10th_Mar_2022

--- python/syntax/html_parser_part_1.py
+++ python/syntax/html_parser_part_1.py
@@ -1,47 +1,39 @@
 import re
 class Parser:
     def removeComment(self):
+        # remove <!-- comment -->
         self.oneline = re.sub("<!--.*?-->", ' ', self.oneline)
 
     def extractTag(self):
-        # self.oneline = re.sub("<\w*>", '')
-        self.tags = re.findall("<.*?>", self.oneline)
+        # find <tag>
         self.tags = re.findall("(?<=<).*?(?=>)", self.oneline)
-        # print(self.tags)
+
+    def parseAttr(self, raw_attributes):
+        if raw_attributes != '':
+            # find attri-attri_attri="attri, attri attri"
+            attributes = re.findall("\S+=[\'\"].*?[\'\"]|\S+", raw_attributes)
+            for attri in attributes:
+                print("-> " + attri.split('=', 1)[0] + " > " + attri.split('=', 1)[1][1:len(attri.split('=', 1)[1]) - 1] if '=' in attri else "-> " + attri + " > " + "None")
     def parseTag(self):
         for tag in self.tags:
             if str(tag).endswith('/'):
-                print("Empty : " + tag[:len(tag)-2])
+                if str(tag).endswith('/'):
+                    print("Empty : " + tag[:len(tag)-2].split()[0])
+                    self.parseAttr(tag[len(str(tag).split()[0]) + 1 : len(tag)-2])
             elif str(tag).startswith('/'):
                 print("End   : " + tag[1:])
             else:
-                attrTag = str(tag).split()
-                print("Start : " + attrTag[0])
-                if len(attrTag) > 1:
-                    for attri in attrTag[1:]:
-                        if '=' in attri:
-                            print("-> " + attri.split('=')[0] + " > " + ((attri.split('=')[1])[1:len(attri.split('=')[1]) - 1]))
-                        else:
-                            print("-> " + attri.split('=')[0] + " > " + "None")
+                print("Start : " + str(tag).split()[0])
+                self.parseAttr(tag[len(str(tag).split()[0]) + 1 :])
+
 
 
     def __init__(self, lines):
-        # put in oneline, to
         self.oneline = ''.join(lines)
-        # print(self.oneline)
-        # print(re.findall("<!--.*?-->", self.oneline))
         self.removeComment()
         self.extractTag()
         self.parseTag()
 
-        # print(self.oneline)
-
-
-# rows = int(input())
-# Parser([input() for x in range(rows)])
-
 if __name__ == '__main__':
-    # lines = ["<start atasdfde><starT attribute=\"test\">","<value attribute='233'>","<!-- </end>","<empty />--></end><Inside /> <!-- <script>-->"]
-    # Parser(lines)
     rows = int(input())
     Parser([input() for x in range(rows)])
